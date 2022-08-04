dd41cabfb7667ae089d833545a4a2d316b867271
Paul-Elian Tabarant <paul-elian.tabarant@octo.com>
2022-08-04 16:59:42 +0200

    tech: refactor tests

--- src/test/java/GossipingTest.java
+++ src/test/java/GossipingTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,87 +9,102 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GossipingTest {
     private final Gossiping gossiping = new Gossiping();
 
-    // Two buses that encounter, or not
-    @Test
-    void gossiped() {
-        List<String> routes = List.of(
-                "2 1",
-                "2 3"
-        );
-        String duration = gossiping.computeDuration(routes);
+    @Nested
+    class TwoBuses {
+        @Test
+        @DisplayName("when two buses with two stops meet at the first stop, it should return 1")
+        void twoBusesWithTwoStopsMeetAtTheFirstStop() {
+            List<String> routes = List.of(
+                    "2 1",
+                    "2 3"
+            );
+            String duration = gossiping.computeDuration(routes);
 
-        assertEquals("1", duration);
-    }
-    @Test
-    void neverGossiped() {
-        List<String> routes = List.of(
-                "2 1",
-                "4 3"
-        );
-        String duration = gossiping.computeDuration(routes);
+            assertEquals("1", duration);
+        }
 
-        assertEquals("never", duration);
-    }
-    @Test
-    void gossiped5() {
-        List<String> routes = List.of(
-                "1 3 2 5 4",
-                "2 4 3 7 4"
-        );
-        String duration = gossiping.computeDuration(routes);
+        @Test
+        @DisplayName("when two buses have different numbers at each stop, it should return 'never'")
+        void neverGossiped() {
+            List<String> routes = List.of(
+                    "2 1",
+                    "4 3"
+            );
+            String duration = gossiping.computeDuration(routes);
 
-        assertEquals("5", duration);
-    }
+            assertEquals("never", duration);
+        }
 
-    // Test with three buses
-    @Test
-    void threeNeverGossiped() {
-        List<String> routes = List.of(
-                "2 1",
-                "4 3",
-                "1 4"
-        );
-        String duration = gossiping.computeDuration(routes);
+        @Test
+        @DisplayName("when two buses have the same stop number for the first time at the stop number 5, it should return 5")
+        void gossiped5() {
+            List<String> routes = List.of(
+                    "1 3 2 5 4",
+                    "2 4 3 7 4"
+            );
+            String duration = gossiping.computeDuration(routes);
 
-        assertEquals("never", duration);
-    }
-    @Test
-    void whenEveryBusEncountersTheOtherOnesInAStopTheyAllHadTimeToGossip() {
-        List<String> routes = List.of(
-                "2 1 3",
-                "4 2 3",
-                "4 1 5"
-        );
-        String duration = gossiping.computeDuration(routes);
-
-        assertEquals("3", duration);
-    }
-    @Test
-    @DisplayName("When one bus encounters the two others and re-encounters one of them, it should return the index when he did the last encounter")
-    void whenOneBusEncountersTheTwoOthersAndReEncountersOneOfThemItShouldReturnTheIndexWhenHeDidThisLastEncounter() {
-        List<String> routes = List.of(
-                "3 1 2 3 3",
-                "3 2 3 1 3",
-                "4 2 3 4 5"
-        );
-        String duration = gossiping.computeDuration(routes);
-
-        assertEquals("5", duration);
+            assertEquals("5", duration);
+        }
     }
 
-    // Extend behaviour to five buses
-    @Test
-    @DisplayName("When five buses have all gossiped after 7 days, it should return 7")
-    void whenFiveBusesHaveAllGossipedAfter7DaysItShouldReturn7() {
-        List<String> routes = List.of(
-                "3 1 2 3 3 3 5",
-                "3 2 4 1 3 5 5",
-                "4 2 7 8 5 3 4",
-                "6 4 2 6 6 7 1",
-                "8 8 3 8 8 2 1"
-        );
-        String duration = gossiping.computeDuration(routes);
+    @Nested
+    class ThreeBuses {
+        @Test
+        @DisplayName("when three buses have different numbers at each stop, it should return 'never'")
+        void threeNeverGossiped() {
+            List<String> routes = List.of(
+                    "2 1",
+                    "4 3",
+                    "1 4"
+            );
+            String duration = gossiping.computeDuration(routes);
 
-        assertEquals("7", duration);
+            assertEquals("never", duration);
+        }
+
+        @Test
+        @DisplayName("when one bus encounters another at stop 1, encounters the last one at stop 2 and the other two meet at stop 3, it should return 3")
+        void whenEveryBusEncountersTheOtherOnesInAStopTheyAllHadTimeToGossip() {
+            List<String> routes = List.of(
+                    "2 1 3",
+                    "4 2 3",
+                    "4 1 5"
+            );
+            String duration = gossiping.computeDuration(routes);
+
+            assertEquals("3", duration);
+        }
+
+        @Test
+        @DisplayName("when one bus encounters the two others at stops 1 et 3 and re-encounters one of them at stop 5, it should return 5")
+        void whenOneBusEncountersTheTwoOthersAndReEncountersOneOfThemItShouldReturnTheIndexWhenHeDidThisLastEncounter() {
+            List<String> routes = List.of(
+                    "3 1 2 3 3",
+                    "3 2 3 1 3",
+                    "4 2 3 4 5"
+            );
+            String duration = gossiping.computeDuration(routes);
+
+            assertEquals("5", duration);
+        }
+    }
+
+    @Nested
+    class NBuses {
+        @Test
+        @DisplayName("when every one of the five buses know the gossips of the four others after 7 days, it should return 7")
+        void whenFiveBusesHaveAllGossipedAfter7DaysItShouldReturn7() {
+            List<String> routes = List.of(
+                    "3 1 2 3 3 3 5",
+                    "3 2 4 1 3 5 5",
+                    "4 2 7 8 5 3 4",
+                    "6 4 2 6 6 7 1",
+                    "8 8 3 8 8 2 1"
+            );
+            String duration = gossiping.computeDuration(routes);
+
+            assertEquals("7", duration);
+        }
     }
 }
